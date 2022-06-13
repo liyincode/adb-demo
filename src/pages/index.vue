db13d19d95e5b2607507b68402fcf130155d8954
liyin <shuaiyin.top@foxmail.com>
2022-06-13 20:51:49 +0800

    feat: add adb connect

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import AdbWebUsbBackend, { AdbWebUsbBackendWatcher } from '@yume-chan/adb-backend-webusb'
-import type { AdbBackend } from '@yume-chan/adb'
+import type { AdbBackend, AdbPacketData, AdbPacketInit } from '@yume-chan/adb'
 // 当前浏览器是否支持
 const supported = ref(false)
 // 获取到的 adb 设备列表
 const backendList = ref<AdbBackend[]>()
 // 当前选择的 adb 设备
 const selectedBackend = ref<AdbBackend>()
+// 是否正在连接
+const connecting = ref(false)
 
 function init() {
   supported.value = AdbWebUsbBackend.isSupported()
@@ -24,21 +26,42 @@ function init() {
       selectedBackend.value = list.find(backend => backend.serial === serial)
   })
 
-  onUnmounted(() => watcher.dispose())
+  // onUnmounted(() => watcher.dispose())
 }
 
 async function updateUsbBackendList() {
   const _backendList = await AdbWebUsbBackend.getDevices()
+  console.log('list', _backendList)
+
   backendList.value = _backendList
 
   return _backendList
 }
 
-init()
-
-const go = () => {
-
+const addUsbBackend = async () => {
+  const backend = await AdbWebUsbBackend.requestDevice()
+  selectedBackend.value = backend
 }
+
+const connect = async () => {
+  if (!selectedBackend.value)
+    return
+
+  connecting.value = true
+
+  let readable: ReadableStream<AdbPacketData>
+  let writable: WritableStream<AdbPacketInit>
+  try {
+    const streams = await selectedBackend.value.connect()
+
+    readable = streams.readable
+  }
+  catch (e: any) {
+    connecting.value = false
+  }
+}
+
+init()
 </script>
 
 <template>
@@ -53,9 +76,16 @@ const go = () => {
     <div>
       <button
         class="m-3 text-sm btn"
-        @click="go"
+        @click="addUsbBackend"
       >
-        Go
+        添加设备
+      </button>
+      <button
+        class="m-3 text-sm btn bg-sky-600"
+        :disabled="connecting"
+        @click="connect"
+      >
+        {{ connecting ? '正在连接请稍后' : '连接设备' }}
       </button>
     </div>
   </div>
