f6dda36af08836bd1b922931c477c137a75afe00
liyin <shuaiyin.top@foxmail.com>
2022-06-14 10:18:30 +0800

    feat: 连接当前设备

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import AdbWebUsbBackend, { AdbWebUsbBackendWatcher } from '@yume-chan/adb-backend-webusb'
+import { Adb } from '@yume-chan/adb'
 import type { AdbBackend, AdbPacketData, AdbPacketInit } from '@yume-chan/adb'
+import AdbWebCredentialStore from '@yume-chan/adb-credential-web'
+
+const CredentialStore = new AdbWebCredentialStore()
+
 // 当前浏览器是否支持
 const supported = ref(false)
 // 获取到的 adb 设备列表
@@ -9,6 +14,8 @@ const backendList = ref<AdbBackend[]>()
 const selectedBackend = ref<AdbBackend>()
 // 是否正在连接
 const connecting = ref(false)
+// 连接成功后的设备
+const device = ref<Adb>()
 
 function init() {
   supported.value = AdbWebUsbBackend.isSupported()
@@ -53,10 +60,37 @@ const connect = async () => {
   let writable: WritableStream<AdbPacketInit>
   try {
     const streams = await selectedBackend.value.connect()
+    console.log('streams', streams)
 
     readable = streams.readable
+    writable = streams.writable
   }
   catch (e: any) {
+    connecting.value = false
+    return
+  }
+
+  async function dispose() {
+    try { readable.cancel() }
+    catch { }
+    try { await writable.close() }
+    catch { }
+  }
+
+  try {
+    const _device = await Adb.authenticate(
+      { readable, writable },
+      CredentialStore,
+      undefined,
+    )
+    console.log('device', _device)
+
+    device.value = _device
+  }
+  catch (e: any) {
+    await dispose()
+  }
+  finally {
     connecting.value = false
   }
 }
@@ -67,12 +101,16 @@ init()
 <template>
   <div>
     <p>{{ supported ? '当前浏览器支持' : '当前浏览器不支持' }}</p>
-    <template v-if="backendList">
-      <div v-for="item in backendList" :key="item.serial">
-        {{ item }}
-      </div>
-    </template>
+    <p>
+      可用设备列表：
+      <template v-if="backendList">
+        <span v-for="item in backendList" :key="item.serial">
+          {{ item.name }}
+        </span>
+      </template>
+    </p>
 
+    <p>已连接设备名称：{{ device?.model ?? '请点击添加设备再点击连接设备' }}</p>
     <div>
       <button
         class="m-3 text-sm btn"
